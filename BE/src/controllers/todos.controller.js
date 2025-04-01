9fcf787dd3b8e1afbcc990dc446b8cddfe02050c
arafatmansuri <mansuriarafat302@gmail.com>
2025-04-01 08:53:41 +0530

    Added Mark as done router and controller

--- BE/src/controllers/todos.controller.js
+++ BE/src/controllers/todos.controller.js
@@ -1,6 +1,6 @@
 const { readFile, writeFile } = require("../db/fileHandler");
 const _ = require("lodash");
-async function users(a) {
+async function getUsers() {
   const data = await readFile();
   const users = await JSON.parse(data);
   return users;
@@ -12,8 +12,7 @@ async function addTodo(req, res) {
       return res.status(402).json({ message: "Todo can't be empty" });
     }
     const user = req.user;
-    const data = await readFile();
-    const users = await JSON.parse(data);
+    const users = await getUsers();
     const currentUserIndex = users.findIndex((value) => {
       return user.username === value.username;
     });
@@ -38,6 +37,44 @@ async function addTodo(req, res) {
       .json({ message: "Something unexpected occured from our side" });
   }
 }
+async function markAsDone(req, res) {
+  try {
+    const todoId = req.params.id;
+    if (todoId === "") {
+      return res.status(402).json({ message: "Todo Id can't be empty" });
+    }
+    const user = req.user;
+    const users = await getUsers();
+    const currentUserIndex = users.findIndex((value) => {
+      return user.username === value.username;
+    });
+    if (currentUserIndex == -1) {
+      return res
+        .status(500)
+        .json({ message: "Somthing went wrong from our side,User not found" });
+    }
+    const currentTaskIndex = users[currentUserIndex].tasks.findIndex(
+      (value) => {
+        return todoId === value.id;
+      }
+    );
+    if (currentTaskIndex == -1) {
+      return res
+        .status(500)
+        .json({ message: "Somthing went wrong from our side,Task not found" });
+    }
+    users[currentUserIndex].tasks[currentTaskIndex].status = true;
+    await writeFile(users);
+    return res.status(200).json({
+      message: "Task Marked as done",
+      task: users[currentUserIndex].tasks[currentTaskIndex],
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Something unexpected occured from our side" });
+  }
+}
 
 async function getAllTodos(req, res) {
   const user = req.user;
@@ -46,4 +83,4 @@ async function getAllTodos(req, res) {
     .json({ message: "Tasks fetched successfully", tasks: user.tasks });
 }
 
-module.exports = { addTodo, getAllTodos };
+module.exports = { addTodo, getAllTodos, markAsDone };
--- BE/src/routes/todos.route.js
+++ BE/src/routes/todos.route.js
@@ -1,10 +1,15 @@
 const { Router } = require("express");
-const { addTodo, getAllTodos } = require("../controllers/todos.controller.js");
+const {
+  addTodo,
+  getAllTodos,
+  markAsDone,
+} = require("../controllers/todos.controller.js");
 const { userAuth } = require("../middlewears/userAuth.middlewear.js");
 
 const todoRouter = Router();
 
 todoRouter.route("/add").post(userAuth, addTodo);
 todoRouter.route("/get").get(userAuth, getAllTodos);
+todoRouter.route("/done/:id").put(userAuth, markAsDone);
 
 module.exports = { todoRouter };
