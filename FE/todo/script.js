ddff5e80bef855da2b230f0978f0d8950d8b0195
arafatmansuri <mansuriarafat302@gmail.com>
2025-04-02 12:19:41 +0530

    Add Logout button and username and added scrollbar

--- FE/todo/script.js
+++ FE/todo/script.js
@@ -1,54 +1,140 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const todoInput = document.getElementById("todo-input");
-  const addTaskButton = document.getElementById("add-task-btn");
-  const todoList = document.querySelector("#todo-list");
-
-  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-  tasks.forEach((element) => {
-    renderTasks(element);
-  });
-  addTaskButton.addEventListener("click", () => {
-    const taskText = todoInput.value.trim();
-    if (taskText === "") return;
-
-    const newTask = {
-      id: Date.now(),
-      text: taskText,
-      completed: false,
-    };
-    tasks.push(newTask);
-    saveTasks();
-    renderTasks(newTask);
-    todoInput.value = "";
-    todoInput.focus();
-    // console.log(tasks);
-  });
-
-  function renderTasks(task) {
-    // console.log(task.text);
-    const li = document.createElement("li");
-    li.setAttribute("data-id", task.id);
-    if (task.completed) li.classList.add("completed");
-    li.innerHTML = `<span>${task.text}</span>
-    <button class="del">delete</button>
-    `;
-    li.addEventListener("click", (e) => {
-      if (e.target.tagName === "BUTTON") return;
-      task.completed = !task.completed;
-      li.classList.toggle("completed");
-      document.querySelector(".del").classList.add("del");
-      saveTasks();
+let todoList = document.querySelector("#todo-list");
+const li = document.querySelector("li");
+document.addEventListener("DOMContentLoaded", async () => {
+  const response = await axios
+    .get("http://127.0.0.1:3000/getuser", {
+      withCredentials: true,
+    })
+    .catch((err) => {
+      const baseUrl = window.location.origin;
+      const addUrl = "/FE";
+      window.location.href = baseUrl + addUrl;
     });
-    li.querySelector("button").addEventListener("click", (e) => {
-      e.stopPropagation(); //prevent toggle from firing
-      tasks = tasks.filter((t) => t.id != task.id);
-      li.remove();
-      saveTasks();
-    });
-    todoList.appendChild(li);
-  }
-  function saveTasks() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTasks();
+  document.querySelector(
+    "h1"
+  ).textContent = `${response.data.user.username}`;
+});
+const Input = document.querySelector("#todo-input");
+const addTask = document.querySelector("#add-task-btn");
+Input.addEventListener("keydown", () => {
+  if (Input.value === "") {
+    addTaskdisabled = true;
+  } else {
+    addTask.disabled = false;
   }
 });
+Input.focus();
+addTask.addEventListener("click", async (e) => {
+  const InputText = Input.value;
+  const response = await axios
+    .post(
+      "http://127.0.0.1:3000/todo/add",
+      { todo: InputText },
+      {
+        withCredentials: true,
+      }
+    )
+    .catch((err) => {
+      if (err.response.status >= 400) {
+        const baseUrl = window.location.origin;
+        const addUrl = "/FE";
+        window.location.href = baseUrl + addUrl;
+      }
+    });
+  Input.value = "";
+  Input.focus();
+});
+
+async function renderTasks() {
+  const response = await axios
+    .get("http://127.0.0.1:3000/todo/get", {
+      withCredentials: true,
+    })
+    .catch((err) => {
+      const baseUrl = window.location.origin;
+      const addUrl = "/FE";
+      window.location.href = baseUrl + addUrl;
+    });
+  const tasks = response.data.tasks;
+  tasks.map((Task) => {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    const Img = document.createElement("img");
+    Img.src = "../resources/uncheck.png";
+    Img.classList.add("uncheck");
+    span.textContent = Task.todo;
+    button.textContent = "delete";
+    li.setAttribute("data-id", Task.id);
+    li.appendChild(Img);
+    li.appendChild(span);
+    li.appendChild(button);
+
+    if (Task.status) {
+      span.classList.add("completed");
+      Img.src = "../resources/check.png";
+      Img.classList.add("check");
+    }
+    todoList.appendChild(li);
+
+    li.addEventListener("click", async (e) => {
+      if (e.target.tagName === "BUTTON") return;
+      let id = "";
+      if (e.target.tagName === "LI") {
+        id = e.target.getAttribute("data-id");
+      } else {
+        id = e.target.parentElement.getAttribute("data-id");
+      }
+      span.classList.toggle("completed");
+      if (span.className.match("completed")) {
+        Img.src = "../resources/check.png";
+        Img.classList.replace("uncheck", "check");
+        // Task.completed = true; Status API
+
+        await axios
+          .put(
+            `http://127.0.0.1:3000/todo/status/${id}`,
+            {},
+            {
+              withCredentials: true,
+            }
+          )
+          .catch((err) => {
+            const baseUrl = window.location.origin;
+            const addUrl = "/FE";
+            window.location.href = baseUrl + addUrl;
+          });
+      } else {
+        Img.src = "../resources/uncheck.png";
+        Img.classList.replace("check", "uncheck");
+        await axios
+          .put(
+            `http://127.0.0.1:3000/todo/status/${id}`,
+            {},
+            {
+              withCredentials: true,
+            }
+          )
+          .catch((err) => {
+            const baseUrl = window.location.origin;
+            const addUrl = "/FE";
+            window.location.href = baseUrl + addUrl;
+          });
+      }
+    });
+    li.querySelector("button").addEventListener("click", async (e) => {
+      e.stopPropagation();
+      const id = e.target.parentElement.getAttribute("data-id");
+      await axios
+        .delete(`http://127.0.0.1:3000/todo/delete/${id}`, {
+          withCredentials: true,
+        })
+        .catch((err) => {
+          const baseUrl = window.location.origin;
+          const addUrl = "/FE";
+          window.location.href = baseUrl + addUrl;
+        });
+    });
+  });
+}
