03e1a2345c4d7665d0344cdc09943091919ec9a9
arafatmansuri <mansuriarafat302@gmail.com>
2025-04-07 18:43:37 +0530

    Add Zod for Input validations in signup controller

--- BE/src/controllers/signup.controller.js
+++ BE/src/controllers/signup.controller.js
@@ -1,24 +1,31 @@
 const { UserModel } = require("../db/index.js");
 const bcrypt = require("bcrypt");
-
+const { z } = require("zod");
 
 async function signup(req, res) {
   try {
-    const { username, password } = req.body;
-    if (
-      [username, password].some((field) => {
-        return field === "";
-      })
-    ) {
-      return res.status(402).json({ message: "All fields are required" });
+    const requiredBody = z.object({
+      username: z.string().min(3).max(30),
+      password: z.string().min(3).max(100),
+    });
+    // const parsedData = requiredBody.parse(req.body)
+    const safeParsedData = requiredBody.safeParse(req.body);
+    if (!safeParsedData.success) {
+      return res.status(402).json({
+        message: "Invalid Format",
+        error: `${
+          safeParsedData.error.errors[0].path[0]
+        }${safeParsedData.error.errors[0].message.replace("String", "")}`,
+      });
     }
+    const { username, password } = req.body;
     const user = await UserModel.findOne({ username: username });
     if (user) {
       return res
         .status(300)
         .json({ message: "User already exists with this username" });
     }
-    const hashedPassword = await bcrypt.hash(password,10)
+    const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await UserModel.create({
       username: username,
       password: hashedPassword,
@@ -27,11 +34,9 @@ async function signup(req, res) {
       .status(200)
       .json({ message: "User signed in successfully", newUser });
   } catch (err) {
-    return res
-      .status(500)
-      .json({
-        message: err.message || "Something went wrong while creating user",
-      });
+    return res.status(500).json({
+      message: err.message || "Something went wrong while creating user",
+    });
   }
 }
 
